0a2e4bb66a693385e975e2b57f19f0e6994495de
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2023-09-26 21:03:14 -0400

    initializing rough draft

---
+++ script.js
@@ -0,0 +1,14 @@
+const grid = document.querySelector('.container');
+
+let createGrid = function() {
+for (let i = 0; i < (16 * 16); i++) {
+    const div = document.createElement('div');
+    div.classList.add('square');
+    div.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black';
+    })
+    grid.appendChild(div); 
+}
+};
+
+createGrid()
