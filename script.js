1e86f92c4fcdf851bb7eb77ad00af9c6192030d7
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2023-10-21 19:39:09 -0400

    functioning multicolor button

--- script.js
+++ script.js
@@ -43,7 +43,25 @@ clear.addEventListener('click', function(){
     }
 });
 
+function getRandomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+}
 
+const rgb = document.querySelector('#rgb');
+rgb.addEventListener('click', function(){
+    let val = document.getElementById('slider').value;
+    let cell = grid.children;
+    for (let i = 0; i < val*val; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = getRandomColor();
+        })
+    }
+});
 
 
 createGrid()
