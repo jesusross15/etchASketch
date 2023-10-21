cee4e9ed317c242d60785aa6a4093800cfc0188a
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2023-10-20 23:59:56 -0400

    slider

--- script.js
+++ script.js
@@ -4,20 +4,34 @@ let createGrid = function() {
 for (let i = 0; i < (16 * 16); i++) {
     const div = document.createElement('div');
     div.classList.add('square');
-    div.addEventListener('mouseover', function(event){
+    div.addEventListener('mouseover', function(event) {
         event.target.style.backgroundColor = 'black';
     })
     grid.appendChild(div); 
 }
 };
 
-const reset = document.querySelector('#reset');
-reset.addEventListener('click', function(){
+const slider = document.querySelector('#slider')
+const screenVal = document.querySelector('.value');
+slider.addEventListener('input', function(){
     let val = document.getElementById('slider').value;
-    let cell = grid.children;
+    screenVal.textContent = val;
+    removeAllChildNodes(grid);
+    grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
     for (let i = 0; i < val*val; i++) {
-        cell[i].style.backgroundColor = 'white';
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        grid.appendChild(div); 
     }
+});
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', function(event) {
+    let val = document.getElementById('slider').value;
+    event.target.style.backgroundColor = 'black';
 });
 
 createGrid()
