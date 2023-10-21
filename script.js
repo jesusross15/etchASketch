5772943bb4097d35826177c6b37c4b091d6b94fe
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2023-10-21 18:05:42 -0400

    working slider, shifted buttons, and small edits

--- script.js
+++ script.js
@@ -11,28 +11,30 @@ for (let i = 0; i < (16 * 16); i++) {
 }
 };
 
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 const slider = document.querySelector('#slider')
-const screenVal = document.querySelector('.value');
-slider.addEventListener('input', function(){
+const gridSize = document.querySelector('.value');
+slider.addEventListener('input', function() {
     let val = document.getElementById('slider').value;
-    screenVal.textContent = val;
+    gridSize.textContent = val;
     removeAllChildNodes(grid);
     grid.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
     for (let i = 0; i < val*val; i++) {
         const div = document.createElement('div');
-        div.classList.add('cell');
-        div.addEventListener('mouseover', function(event){
+        div.classList.add('square');
+        div.addEventListener('mouseover', function(event) {
             event.target.style.backgroundColor = 'black';
         })
         grid.appendChild(div); 
     }
 });
 
-const reset = document.querySelector('#reset');
-reset.addEventListener('click', function(event) {
-    let val = document.getElementById('slider').value;
-    event.target.style.backgroundColor = 'black';
-});
+
 
 createGrid()
 
